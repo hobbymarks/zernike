ea63316ee18abf1f72bf3952c41e809ae11da5be
Rod Conan <rconan@gmto.org>
2021-01-29 20:26:50 -0800

    MODIFIED GRAM-SCHMIDT
    Orthonormalized Zernike mode set with the modified Gram-Schmidt algorithm

--- examples/main.rs
+++ examples/main.rs
@@ -6,9 +6,6 @@ fn main() {
     let n_radial_order = 11;
     let (j, n, m) = zernike::jnm(n_radial_order);
     println!("{:2} {:2} {:2}", "n", "m", "j");
-    (0..j.len()).for_each(|k| println!("{:2} {:2} {:2}", n[k], m[k], j[k]));
-    let zern = zernike::mode_set(n_radial_order, n_xy);
-
     let d = 2f64 / (n_xy - 1) as f64;
     let h = ((n_xy - 1) / 2) as f64;
     let xy: Vec<_> = (0..n_xy * n_xy)
@@ -20,23 +17,50 @@ fn main() {
             (x, y)
         })
         .collect();
+    (0..j.len()).for_each(|k| println!("{:2} {:2} {:2}", n[k], m[k], j[k]));
 
-    zern.chunks(n_xy * n_xy).enumerate().for_each(|(k, z)| {
-        let filename = format!("examples/zernike_{}.png", j[k]);
-        let plot = BitMapBackend::new(&filename, (n_xy as u32, n_xy as u32)).into_drawing_area();
-        plot.fill(&WHITE).unwrap();
-        let chart = ChartBuilder::on(&plot)
-            .build_cartesian_2d(-1f64..1f64, -1f64..1f64)
-            .unwrap();
-        let plotting_area = chart.plotting_area();
-        let z_max = z.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
-        let z_min = z.iter().cloned().fold(f64::INFINITY, f64::min);
-        //println!("Min-Max: [{:.3};{:.3}]",z_min,z_max);
-        let uz: Vec<f64> = z.iter().map(|p| (p - z_min) / (z_max - z_min)).collect();
-        uz.iter().zip(xy.iter()).for_each(|(z, xy)| {
-            plotting_area
-                .draw_pixel(*xy, &HSLColor(0.5 * z, 0.5, 0.4))
+    {
+        let zern = zernike::mode_set(n_radial_order, n_xy);
+        zern.chunks(n_xy * n_xy).enumerate().for_each(|(k, z)| {
+            let filename = format!("examples/zernike_{}.png", j[k]);
+            let plot =
+                BitMapBackend::new(&filename, (n_xy as u32, n_xy as u32)).into_drawing_area();
+            plot.fill(&WHITE).unwrap();
+            let chart = ChartBuilder::on(&plot)
+                .build_cartesian_2d(-1f64..1f64, -1f64..1f64)
                 .unwrap();
-        })
-    });
+            let plotting_area = chart.plotting_area();
+            let z_max = z.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
+            let z_min = z.iter().cloned().fold(f64::INFINITY, f64::min);
+            //println!("Min-Max: [{:.3};{:.3}]",z_min,z_max);
+            let uz: Vec<f64> = z.iter().map(|p| (p - z_min) / (z_max - z_min)).collect();
+            uz.iter().zip(xy.iter()).for_each(|(z, xy)| {
+                plotting_area
+                    .draw_pixel(*xy, &HSLColor(0.5 * z, 0.5, 0.4))
+                    .unwrap();
+            })
+        });
+    }
+    {
+        let zern = zernike::mgs_mode_set(n_radial_order, n_xy);
+        zern.chunks(n_xy * n_xy).enumerate().for_each(|(k, z)| {
+            let filename = format!("examples/msg_zernike_{}.png", j[k]);
+            let plot =
+                BitMapBackend::new(&filename, (n_xy as u32, n_xy as u32)).into_drawing_area();
+            plot.fill(&WHITE).unwrap();
+            let chart = ChartBuilder::on(&plot)
+                .build_cartesian_2d(-1f64..1f64, -1f64..1f64)
+                .unwrap();
+            let plotting_area = chart.plotting_area();
+            let z_max = z.iter().cloned().fold(f64::NEG_INFINITY, f64::max);
+            let z_min = z.iter().cloned().fold(f64::INFINITY, f64::min);
+            //println!("Min-Max: [{:.3};{:.3}]",z_min,z_max);
+            let uz: Vec<f64> = z.iter().map(|p| (p - z_min) / (z_max - z_min)).collect();
+            uz.iter().zip(xy.iter()).for_each(|(z, xy)| {
+                plotting_area
+                    .draw_pixel(*xy, &HSLColor(0.5 * z, 0.5, 0.4))
+                    .unwrap();
+            })
+        });
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -53,7 +53,7 @@ pub fn gamma(x: f64) -> f64 {
 }
 /// Zernike radial function
 fn radial(n: u32, m: u32, r: f64) -> f64 {
-    (0..(n - m) / 2+1).fold(0f64, |a, k| {
+    (0..(n - m) / 2 + 1).fold(0f64, |a, k| {
         a + (-1f64).powf(k as f64) * gamma((n - k + 1) as f64) * r.powf((n - 2 * k) as f64)
             / (gamma((k + 1) as f64)
                 * gamma(((n + m) / 2 - k + 1) as f64)
@@ -96,6 +96,43 @@ pub fn mode_set(n_radial_order: u32, n_xy: usize) -> Vec<f64> {
         .zip(n.iter().zip(m.iter()))
         .flat_map(|(&j, (&n, &m))| mode(j, n, m, n_xy))
         .collect()
+}
+pub fn mgs_mode_set(n_radial_order: u32, n_xy: usize) -> Vec<f64> {
+    let n = n_xy * n_xy;
+    let nz = n_radial_order * (n_radial_order + 1) / 2;
+    let v: Vec<Vec<f64>> = mode_set(n_radial_order, n_xy)
+        .chunks(n)
+        .map(|x| x.to_vec())
+        .collect();
+    let mut u: Vec<Vec<f64>> = vec![0f64; n * nz as usize]
+        .chunks(n)
+        .map(|x| x.to_vec())
+        .collect();
+    let norm = |x: &[f64], y: &[f64]| {
+        x.iter()
+            .zip(y.iter())
+            .fold(0f64, |a, (x, y)| a + x * y)
+    };
+    let nrm = norm(&v[0], &v[0]).sqrt();
+    u[0].iter_mut().zip(v[0].iter()).for_each(|(u, v)| {
+        *u = v / nrm;
+    });
+    (1..nz as usize).for_each(|i| {
+        u[i].iter_mut().zip(v[i].iter()).for_each(|(u, v)| {
+            *u = *v;
+        });
+        (0..i - 1).for_each(|j| {
+            let r = norm(&u[j], &u[i]) / norm(&u[j], &u[j]);
+            u[i].iter_mut().zip(v[j].iter()).for_each(|(u, v)| {
+                *u -= r * v;
+            });
+            let nrm = norm(&v[i], &v[i]).sqrt();
+            u[i].iter_mut().for_each(|u| {
+                *u /= nrm;
+            });
+        });
+    });
+    u.iter().flat_map(|x| x.to_vec()).collect()
 }
 /// Returns the Zernike indices `(j,n,m)` for the first `n_radial_order`s
 pub fn jnm(n_radial_order: u32) -> (Vec<u32>, Vec<u32>, Vec<u32>) {
